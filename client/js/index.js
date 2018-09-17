fb427c877cff26289513ef5fe8e347c1aa966679
Kirill Vinogradov <vikian050194@gmail.com>
2018-09-17 21:01:12 +0300

    Change UI;

--- client/js/index.js
+++ client/js/index.js
@@ -4,39 +4,52 @@ import RandomInt from "random-int";
 $(document).ready(function () {
     let countOfElements = 5;
     let content = "";
-    let animationDuration = 3000;
+    let animationDurationInSeconds = 5;
     let firstId = null;
     let secondId = null;
-    const nameOfAnimetionForLeftElement = "animetionForLeftElement";
-    const nameOfAnimetionForRightElement = "animetionForRightElement";
+    const height = 100;
+    const nameOfAnimationForLeftElement = "animationForLeftElement";
+    const nameOfAnimationForRightElement = "animationForRightElement";
     const animationForLeftElement = `@keyframes {NAME} {
     0% {
+        border-color: black;
+        left: 0px;
+        top: 0px;
+    }
+
+    20% {
         border-color: orangered;
         left: 0px;
         top: 0px;
     }
 
-    33% {
+    40% {
         border-color: gold;
         left: 0px;
         top: {Y}px;
     }
 
-    66% {
+    60% {
         border-color: deepskyblue;
         left: {X}px;
         top: {Y}px;
     }
 
-    100% {
+    80% {
         border-color: yellowgreen;
+        left: {X}px;
+        top: 0px;
+    }
+
+    100% {
+        border-color: black;
         left: {X}px;
         top: 0px;
     }
 }`;
 
     for (let i = 0; i < countOfElements; i++) {
-        content += `<div id="${i}" class="element" style="order:${i};">${RandomInt(1, 99)}</div>`;
+        content += `<h2 id="${i}" class="element" style="order:${i};">${RandomInt(1, 99)}</h2>`;
     }
 
     $(".elements-container").html(content);
@@ -52,20 +65,20 @@ $(document).ready(function () {
 
                 let style = document.createElement('style');
                 style.type = 'text/css';
-                style.innerHTML = animationForLeftElement.replace("{NAME}", nameOfAnimetionForLeftElement);
+                style.innerHTML = animationForLeftElement.replace("{NAME}", nameOfAnimationForLeftElement);
                 style.innerHTML = style.innerHTML.replace(/\{X\}/g, "240");
-                style.innerHTML = style.innerHTML.replace(/\{Y\}/g, "200");
+                style.innerHTML = style.innerHTML.replace(/\{Y\}/g, height);
                 document.getElementsByTagName('head')[0].appendChild(style);
 
                 style = document.createElement('style');
                 style.type = 'text/css';
-                style.innerHTML = animationForLeftElement.replace("{NAME}", nameOfAnimetionForRightElement);
+                style.innerHTML = animationForLeftElement.replace("{NAME}", nameOfAnimationForRightElement);
                 style.innerHTML = style.innerHTML.replace(/\{X\}/g, "-240");
-                style.innerHTML = style.innerHTML.replace(/\{Y\}/g, "-200");
+                style.innerHTML = style.innerHTML.replace(/\{Y\}/g, -height);
                 document.getElementsByTagName('head')[0].appendChild(style);
 
-                $(`#${firstId}`).css("animation-name", nameOfAnimetionForLeftElement);
-                $(`#${secondId}`).css("animation-name", nameOfAnimetionForRightElement);
+                $(`#${firstId}`).css("animation-name", nameOfAnimationForLeftElement);
+                $(`#${secondId}`).css("animation-name", nameOfAnimationForRightElement);
 
                 setTimeout(() => {
                     $(`#${firstId}`).css("order", secondId);
@@ -76,7 +89,7 @@ $(document).ready(function () {
 
                     firstId = null;
                     secondId = null;
-                }, animationDuration);
+                }, animationDurationInSeconds * 1000);
             }
         });
     });
