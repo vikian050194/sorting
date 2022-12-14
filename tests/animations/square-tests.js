452528bbc0689d5f228f11d7c0a0ddc0cad5d857
Kirill Vinogradov <vikian050194@gmail.com>
2022-12-14 06:52:10 +0100

    100% square animation coverage

--- tests/animations/square-tests.js
+++ tests/animations/square-tests.js
@@ -5,7 +5,14 @@ import { SquareAnimation } from "../../src/js/animations";
 const square = new SquareAnimation();
 const gen = square.swap();
 
-describe("Animations: sqare", function () {
+describe("Animations: square", function () {
+    it("Key", function () {
+        const actual = square.key;
+        const expected = "square";
+
+        assert.equal(actual, expected);
+    });
+
     it("Zero progress", function () {
         const index = 0;
         const action = new SwapAction(0, 1);
@@ -20,7 +27,35 @@ describe("Animations: sqare", function () {
         assert.deepEqual(actual, expected);
     });
 
-    it("50% passed", function () {
+    it("10% progress: from", function () {
+        const index = 0;
+        const action = new SwapAction(0, 1);
+        const progress = 0.1;
+        const actual = gen(index, action, progress);
+        const expected = {
+            order: 0,
+            bottom: "16px",
+            left: "0px"
+        };
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("10% progress: to", function () {
+        const index = 1;
+        const action = new SwapAction(0, 1);
+        const progress = 0.1;
+        const actual = gen(index, action, progress);
+        const expected = {
+            order: 1,
+            bottom: "-16px",
+            left: "0px"
+        };
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("50% progress", function () {
         const index = 0;
         const action = new SwapAction(0, 1);
         const progress = 0.5;
@@ -34,7 +69,21 @@ describe("Animations: sqare", function () {
         assert.deepEqual(actual, expected);
     });
 
-    it("Completed", function () {
+    it("80% progress", function () {
+        const index = 0;
+        const action = new SwapAction(0, 1);
+        const progress = 0.87;
+        const actual = gen(index, action, progress);
+        const expected = {
+            order: 0,
+            bottom: "20.8px",
+            left: "80px"
+        };
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("100% progress: from", function () {
         const index = 0;
         const action = new SwapAction(0, 1);
         const progress = 1;
@@ -42,6 +91,34 @@ describe("Animations: sqare", function () {
         const expected = {
             order: 1,
             bottom: "0px",
+            left: "0px"
+        };
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("100% progress: to", function () {
+        const index = 1;
+        const action = new SwapAction(0, 1);
+        const progress = 1;
+        const actual = gen(index, action, progress);
+        const expected = {
+            order: 0,
+            bottom: "0px",
+            left: "0px"
+        };
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("10% progress: delta > 1", function () {
+        const index = 0;
+        const action = new SwapAction(0, 2);
+        const progress = 0.1;
+        const actual = gen(index, action, progress);
+        const expected = {
+            order: 0,
+            bottom: "32px",
             left: "0px"
         };
 
